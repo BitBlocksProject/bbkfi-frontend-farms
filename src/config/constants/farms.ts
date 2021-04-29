3cf369d99eb65c6c847586c20ecfc2198c581235
SzhenBBK <edsoneto.50@gmail.com>
2021-04-29 19:41:57 -0300

    Fix wrong TVL calculation

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -176,8 +176,8 @@ const farms: FarmConfig[] = [
       97: '',
       56: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
     },
-    quoteTokenSymbol: QuoteToken.BNB,
-    quoteTokenAdresses: contracts.wbnb,
+    quoteTokenSymbol: QuoteToken.BBKFI,
+    quoteTokenAdresses: contracts.cake,
   },
   {
     pid: 8,
